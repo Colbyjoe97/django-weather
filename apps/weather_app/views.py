73d892704fb5b3ccc1a83ca55f88ba3b6b0e3f3f
colbyjoe97 <dumonchelle123@gmail.com>
2021-11-04 17:03:26 -0400

    started API integration

--- apps/weather_app/views.py
+++ apps/weather_app/views.py
@@ -1,14 +1,38 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import *
-import bcrypt
+import bcrypt, urllib.request, json
 
 # Create your views here.
 def index(request):
+    # city = request.POST['city']
+    url = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=sykesville&appid=5ef4efe8fd1e3cf0ddbcee4989acfeda').read()
+    data_list = json.loads(url)
+    data = {
+        'city': 'Sykesville',
+        "country_code": str(data_list['sys']['country']),
+        'temp': str(data_list['main']['temp']) + '°C',
+    }   
     context={
-        'users': User.objects.all()
+        'users': User.objects.all(),
+        'weather': data,
     }
     return render(request, 'index.html', context)
+
+# def weather(request):
+#     city = request.POST['city']
+#     url = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=sykesville&appid=5ef4efe8fd1e3cf0ddbcee4989acfeda').read()
+#     data_list = json.loads(url)
+#     data = {
+#         'city': city,
+#         "country_code": str(data_list['sys']['country']),
+#         'temp': str(data_list['main']['temp']) + ' °C',
+#     }   
+#     context={
+#         'users': User.objects.all(),
+#         'weather': data,
+#     }
+#     return redirect('/')
 
 def login(request):
     return render(request, 'login.html')
