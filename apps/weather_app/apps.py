3ac776695618411db10cf564f90fd3f47acb51d3
colby <dumonchelle123@gmail.com>
2021-11-11 14:06:51 -0500

    got everything working on laptop

--- apps/weather_app/apps.py
+++ apps/weather_app/apps.py
@@ -1,5 +1,5 @@
-from django.apps import AppConfig
+# from django.apps import AppConfig
 
 
-class WeatherAppConfig(AppConfig):
-    name = 'weather_app'
+# class WeatherAppConfig(AppConfig):
+#     name = 'weather_app'
